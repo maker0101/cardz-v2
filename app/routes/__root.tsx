905b6f1fea97ba3a53a92fa8017b3dfef44d561b
Max <88189468+maker0101@users.noreply.github.com>
2025-07-06 15:17:11 +0200

    Rename project from "ztunes" to "cardz" and update related configurations, including environment variables, package files, and Docker scripts.

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -20,7 +20,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'ztunes',
+        title: 'cardz',
       },
     ],
   }),
--- scripts/clean.ts
+++ scripts/clean.ts
@@ -3,13 +3,13 @@ import {exec} from 'shared/exec';
 console.log('Cleaning up resources...');
 
 try {
-  exec('rm -f /tmp/ztunes.db*');
+  exec('rm -f /tmp/cardz.db*');
 } catch (err) {
   console.info(err.message);
 }
 
 try {
-  exec('docker rm -f ztunes');
+  exec('docker rm -f cardz');
 } catch (err) {
   console.info(err.message);
 }
